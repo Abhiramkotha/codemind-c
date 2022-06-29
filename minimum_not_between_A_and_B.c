aed7e556c1045af2ae3d905b0e8d3b6040ee4728
Abhiramkotha <97886970+Abhiramkotha@users.noreply.github.com>
2022-06-29 10:35:57 +0530

    #include<stdio.h> int main() {     int n,a,b,ar[100],min=99,c=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     for(int i=0;i<n;i++)     {         if(!(ar[i]>=a&&ar[i]<=b))         {           if(ar[i]<min)           {           min=ar[i];           c++;           }         }     }     if(c==0)     printf("-1");     else     printf("%d",min); }

--- minimum_not_between_A_and_B.c
+++ minimum_not_between_A_and_B.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,n,arr[100],i,sum=0,min=9999;
+    int n,a,b,ar[100],min=99,c=0;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-         scanf("%d",&arr[i]);
+        scanf("%d",&ar[i]);
     }
     scanf("%d%d",&a,&b);
-    for(i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        if(!(arr[i]>=a && arr[i]<=b))
-        
+        if(!(ar[i]>=a&&ar[i]<=b))
         {
-            if(arr[i]<min)
-            {
-                min=arr[i];
-                sum++;
-            }
+          if(ar[i]<min)
+          {
+          min=ar[i];
+          c++;
+          }
         }
     }
-    if(sum==0)
+    if(c==0)
     printf("-1");
     else
     printf("%d",min);
